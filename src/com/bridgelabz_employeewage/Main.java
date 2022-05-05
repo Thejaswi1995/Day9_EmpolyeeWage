b6ab9f2d4a631ca442a3c495a94f136b28cf565d
Thejaswi <thejaswirl187@gmail.com>
2022-05-05 16:52:45 +0530

    Added: Employees Wage is calaculated through switch

--- src/com/bridgelabz_employeewage/Main.java
+++ src/com/bridgelabz_employeewage/Main.java
@@ -1,7 +1,7 @@
-package com.bridgelabz_employeewage;
+package com.bridgelabz.employeewage;
 import java.util.Random;
-
 public class Main {
+
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
@@ -11,24 +11,24 @@ public class Main {
     private static void wageComputation() {
         Random random = new Random();
         int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-        if (empCheck == IS_FULL_TIME) {
-            System.out.println("Employee is Present ");
-            empHrs = 8;
-        } else if (empCheck == IS_PART_TIME) {
-            System.out.println("Employee is Present Part Time ");
-            empHrs = 4;
-
-        } else {
-            System.out.println("Employee is Absent ");
+        switch (empCheck) {
+            case IS_FULL_TIME:
+                System.out.println("Employee is Present Full Time");
+                empHrs = 8;
+                break;
+            case IS_PART_TIME:
+                System.out.println("Employee is Present Part Time ");
+                empHrs = 4;
+                break;
+            default:
+                System.out.println("Employee is Absent ");
         }
-
-        int empWage = empHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Employee Wage is : " + empWage);
     }
+    int empWage = empHrs * EMP_RATE_PER_HOUR;
 
     public static void main(String[] args) {
+
         System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
         wageComputation();
     }
 }
-
