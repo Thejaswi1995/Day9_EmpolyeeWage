1f9f649a7cdb16d8c0b6158750bcae87bd85c372
Thejaswi <thejaswirl187@gmail.com>
2022-05-05 16:43:20 +0530

    Added: Employees Wage is calculated

--- src/com/bridgelabz_employeewage/Main.java
+++ src/com/bridgelabz_employeewage/Main.java
@@ -2,17 +2,22 @@ package com.bridgelabz_employeewage;
 import java.util.Random;
 
 public class Main {
-    public static final int IS_PRESENT = 1;
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
 
     public static int empHrs = 0;
 
     private static void wageComputation() {
         Random random = new Random();
-        int empCheck = (int) Math.floor(Math.random() * 10) % 2;
-        if (empCheck == IS_PRESENT) {
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        if (empCheck == IS_FULL_TIME) {
             System.out.println("Employee is Present ");
             empHrs = 8;
+        } else if (empCheck == IS_PART_TIME) {
+            System.out.println("Employee is Present Part Time ");
+            empHrs = 4;
+
         } else {
             System.out.println("Employee is Absent ");
         }
